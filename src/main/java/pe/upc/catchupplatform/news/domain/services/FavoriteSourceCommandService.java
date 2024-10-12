2aa79b4aa2b2ad3dddc7e11e51ccb08468213e8a
DALILA <dalila7300@gmail.com>
2024-10-11 23:17:08 -0500

    feat: add file FavoriteSourceCommandService

--- src/main/java/pe/upc/catchupplatform/news/domain/services/FavoriteSourceCommandService.java
+++ src/main/java/pe/upc/catchupplatform/news/domain/services/FavoriteSourceCommandService.java
@@ -1,4 +1,15 @@
 package pe.upc.catchupplatform.news.domain.services;
 
+import pe.upc.catchupplatform.news.domain.model.aggregates.FavoriteSource;
+import pe.upc.catchupplatform.news.domain.model.commands.GetFavoriteSourceByIdCommand;
+import pe.upc.catchupplatform.news.domain.model.commands.GetFavoriteSourceCommand;
+
+
+import java.util.List;
+import java.util.Optional;
+
 public interface FavoriteSourceCommandService {
+    Optional<FavoriteSource> handle(GetFavoriteSourceByIdCommand command);
+
+    Optional<List<FavoriteSource>> handle(GetFavoriteSourceCommand command);
 }
